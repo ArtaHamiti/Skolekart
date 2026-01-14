b947b7e02c45a5ff500d97a78a5d31b8dd2ad378
Arta Hamiti <a-hamiti@hotmail.com>
2026-01-14 11:36:28 +0100

    La til grunnskoler i kartet

--- kart.py
+++ kart.py
@@ -9,6 +9,7 @@ m = fm.Map(location=Norway_coords, zoom_start=7)
 # Lag ulike grupper
 private_skoler = fm.FeatureGroup(name="Private skoler")
 offentlige_skoler = fm.FeatureGroup(name="Offentlige skoler")
+private_grunnskoler = fm.FeatureGroup(name="Private grunnskoler")
 
 df = pd.read_csv("privatskoler_vgs.csv", comment="#")
 for _, row in df.iterrows():
@@ -18,7 +19,19 @@ for _, row in df.iterrows():
         icon=fm.Icon(color="blue", icon="graduation-cap", prefix="fa")
     ).add_to(private_skoler)
 
+df = pd.read_csv("private_grunnskoler.csv", comment="#")
+for _, row in df.iterrows():
+    fm.Marker(
+        location=[row["Breddegrad"], row["Lengdegrad"]],
+        popup=row["Navn"],
+        icon=fm.Icon(color="blue", icon="school", prefix="fa")
+    ).add_to(private_grunnskoler)
+
 # Legg gruppen til kartet
 private_skoler.add_to(m)
+private_grunnskoler.add_to(m)
 
-m.save("norgeskart_med_manuelt_fylt_inn_for_0_verdier.html")
+# Legg til lagkontroll
+fm.LayerControl().add_to(m)
+
+m.save("norgeskart_med_private_vgs_manuelt_fylt_inn_for_0_verdier_og_private_grunnskoler.html")
